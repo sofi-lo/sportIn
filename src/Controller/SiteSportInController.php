0e13562a3fcd88a2977945f0500e656d6d6d554d
bruno <br.plonquet@gmail.com>
2021-02-26 16:06:57 +0100

    sauve liens accueil

--- src/Controller/SiteSportInController.php
+++ src/Controller/SiteSportInController.php
@@ -8,10 +8,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SiteSportInController extends AbstractController
 {
-    #[Route('/', name: 'site_sport_in')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
         return $this->render('site_sport_in/index.html.twig', [
+            'controller_name' => 'SiteSportInController',
+        ]);
+    }
+
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('site_sport_in/contact.html.twig', [
+            'controller_name' => 'SiteSportInController',
+        ]);
+    }
+
+    #[Route('/concept', name: 'concept')]
+    public function concept(): Response
+    {
+        return $this->render('site_sport_in/concept.html.twig', [
+            'controller_name' => 'SiteSportInController',
+        ]);
+    }
+
+    #[Route('/blog', name: 'blog')]
+    public function blog(): Response
+    {
+        return $this->render('site_sport_in/blog.html.twig', [
             'controller_name' => 'SiteSportInController',
         ]);
     }
