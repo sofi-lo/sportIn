548b7c03853933af880610c55954ad03f78196f5
sophie <sophie@mail.com>
2021-02-26 15:07:40 +0100

    crud

---
+++ src/Form/ChallengeType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Challenge;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ChallengeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('type')
+            ->add('pseudo')
+            ->add('participant')
+            ->add('organisateur')
+            ->add('xpWinner')
+            ->add('arbitre')
+            ->add('createdAt')
+            ->add('dateValidation')
+            ->add('dateFin')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Challenge::class,
+        ]);
+    }
+}
