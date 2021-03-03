476ff8d7531d16b84116abc747cbc4731a1c16ca
sophie <sophie@mail.com>
2021-03-03 16:43:48 +0100

    profil1643

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/category')]
+#[Route('/category')]
 class CategoryController extends AbstractController
 {
     #[Route('/', name: 'category_index', methods: ['GET'])]
