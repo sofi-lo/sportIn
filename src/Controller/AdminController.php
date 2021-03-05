781120158c482ae6d45d9d3c589d4e5815968484
bruno <br.plonquet@gmail.com>
2021-03-05 11:37:09 +0100

    sauve

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class AdminController extends AbstractController
+class AdminController extends AbstractController   
 {
     #[Route('/admin', name: 'admin')]
     public function index(): Response
